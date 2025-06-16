57a1b4b77e506bbbde77b99a3e6d94e984a517c5
RWinkler84 <winkler.ralf84@hotmail.de>
2025-06-16 15:22:04 +0200

    - build a html frontend, styled with css and working js logic for simple operations and operation chainging

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,20 +1,141 @@
-let numberA
-let numberB
-let operator
+const displayContent = document.querySelector('#displayContent');
+
+let numberA = null;
+let numberB = null;
+let operator = null;
+let result;
+
+const awaitingInput = '0';
 
 
-function add(numberA, numberB) {
-    return numberA + numberB;
+//calculations
+function calcAdd() {
+    return Number(numberA) + Number(numberB);
 }
 
-function subtract(numberA, numberB) {
-    return numberA - numberB;
+function calcSubtract() {
+    return Number(numberA) - Number(numberB);
 }
 
-function multiply(numberA, numberB) {
-    return numberA * numberB;
+function calcMultiply() {
+    return Number(numberA) * Number(numberB);
 }
 
-function divide(numberA, numberB) {
-    return numberA / numberB;
- }
+function calcDivide() {
+    return Number(numberA) / Number(numberB);
+}
+
+//other operations
+function runCalculation() {
+    switch (operator) {
+        case '+':
+            result = calcAdd();
+            displayResult(result);
+            clearCurrentCalculation();
+            break;
+
+        case '-':
+            result = calcSubtract();
+            displayResult(result);
+            clearCurrentCalculation();
+            break;
+
+        case '*':
+            result = calcMultiply();
+            displayResult(result);
+            clearCurrentCalculation();
+            break;
+
+        case '/':
+            result = calcDivide();
+            displayResult(result);
+            clearCurrentCalculation();
+            break;
+    }
+}
+
+function runEquals() {
+    if (!numberA && numberB && operator) result ? numberA = result : numberA = 0;  
+
+    if (numberA && numberB && operator) {
+        runCalculation();
+        clearCurrentCalculation();
+    }
+}
+
+function displayResult(result) {
+    displayContent.textContent = result;
+}
+
+function clearCurrentNumber() {
+    operator ? numberB = null : numberA = null;
+}
+
+function clearCurrentCalculation() {
+    numberA = null;
+    numberB = null;
+    operator = null;
+}
+
+function clearFullMemory() {
+    numberA = null;
+    numberB = null;
+    operator = null;
+    result = null;
+}
+
+//input event handlers
+function handleNumberInput(event) {
+    let numberPressed = event.target.textContent;
+
+    if (!operator) {
+        numberA = !numberA ? numberPressed : numberA + numberPressed;
+        if (Number(numberA) == 0) numberA = '0';
+        displayContent.textContent = numberA;
+    }
+
+    if (operator) {
+        numberB = !numberB ? numberPressed : numberB + numberPressed;
+        if (Number(numberB) == 0) numberB = '0';
+        displayContent.textContent = numberB;
+    }
+}
+
+function handleOperatorInput(event) {
+    let operatorPressed = event.target.textContent;
+
+    if (!operator) operator = operatorPressed;
+    if (!numberA) result ? numberA = result : numberA = '0';
+
+    if (numberA && operator && !numberB) operator = operatorPressed;
+
+    if (operator && numberA && numberB) {
+        runCalculation();
+        operator = operatorPressed;
+    }
+}
+
+function handleLowerButtonsInput(event) {
+    let lowerButtonPressed = event.target.textContent;
+
+    switch (lowerButtonPressed) {
+        case '=':
+            runEquals();
+            break;
+
+        case 'C':
+            clearCurrentNumber();
+            displayContent.textContent = awaitingInput;
+            break;
+
+        case 'CE':
+            clearFullMemory();
+            displayContent.textContent = awaitingInput;
+            break;
+    }
+
+}
+
+document.querySelector('#numberButtonContainer').addEventListener('click', handleNumberInput);
+document.querySelector('#operatorButtonContainer').addEventListener('click', handleOperatorInput);
+document.querySelector('#lowerButtonsContainer').addEventListener('click', handleLowerButtonsInput);
