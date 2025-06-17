de24a53bb9048276f86e3b7ec3c075f9a73c7288
RWinkler84 <winkler.ralf84@hotmail.de>
2025-06-17 13:30:09 +0200

    - started working on a landing page for TOP assignements

---
+++ index.js
@@ -0,0 +1,9 @@
+const items = {
+    'landingPage': {
+        'title': 'TOP Landing Page Assignement',
+        'description': 'There is not much to say about this. It is a simply static web page, styled with CSS according to the assignment.',
+        'keyFeatues': ['looks nice I guess'],
+        'previewSource': '',
+        'href': ''
+    }
+}
